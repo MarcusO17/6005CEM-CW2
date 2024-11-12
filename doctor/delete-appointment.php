7b6938624f8c9525b7fbc343726428ad82893301
Lim Ni Feih <p22013772@student.newinti.edu.my>
2024-11-12 23:39:53 +0800

    encoding for doctor delete appointment

--- doctor/delete-appointment.php
+++ doctor/delete-appointment.php
@@ -15,12 +15,11 @@
     if($_GET){
         //import database
         include("../connection.php");
-        $id=$_GET["id"];
-        //$result001= $database->query("select * from schedule where scheduleid=$id;");
-        //$email=($result001->fetch_assoc())["docemail"];
+         // Sanitize and validate the id parameter from GET
+        $id = isset($_GET["id"]) ? filter_var($_GET["id"], FILTER_VALIDATE_INT) : null;
+        
         $sql= $database->query("delete from appointment where appoid='$id';");
-        //$sql= $database->query("delete from doctor where docemail='$email';");
-        //print_r($email);
+
         header("location: appointment.php");
     }
 
