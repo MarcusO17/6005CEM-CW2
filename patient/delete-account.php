f9a1c26ea03da36263a211a86bb91c2844b20607
Lim Ni Feih <p22013772@student.newinti.edu.my>
2024-11-13 19:20:48 +0800

    encode user detail

--- patient/delete-account.php
+++ patient/delete-account.php
@@ -22,20 +22,20 @@
     $stmt->execute();
     $userrow = $stmt->get_result();
     $userfetch=$userrow->fetch_assoc();
-    $userid= $userfetch["pid"];
-    $username=$userfetch["pname"];
-
+    $userid = htmlspecialchars($userfetch["pid"], ENT_QUOTES, 'UTF-8');
+    $username = htmlspecialchars($userfetch["pname"], ENT_QUOTES, 'UTF-8');
     
     if($_GET){
         //import database
         include("../connection.php");
-        $id=$_GET["id"];
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
         $sqlmain= "select * from patient where pid=?";
         $stmt = $database->prepare($sqlmain);
         $stmt->bind_param("i",$id);
         $stmt->execute();
         $result001 = $stmt->get_result();
         $email=($result001->fetch_assoc())["pemail"];
+        $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
 
         $sqlmain= "delete from webuser where email=?;";
         $stmt = $database->prepare($sqlmain);
