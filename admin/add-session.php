da36cc079768f5054dbb9cc6976967a4b4645357
Lim Ni Feih <p22013772@student.newinti.edu.my>
2024-11-13 23:17:56 +0800

    encode and sanitize add session for admin

--- admin/add-session.php
+++ admin/add-session.php
@@ -15,14 +15,15 @@
     if($_POST){
         //import database
         include("../connection.php");
-        $title=$_POST["title"];
-        $docid=$_POST["docid"];
-        $nop=$_POST["nop"];
-        $date=$_POST["date"];
-        $time=$_POST["time"];
+        // Sanitize inputs
+        $title = htmlspecialchars(trim($_POST["title"]), ENT_QUOTES, 'UTF-8');
+        $docid = filter_input(INPUT_POST, 'docid', FILTER_VALIDATE_INT);
+        $nop = filter_input(INPUT_POST, 'nop', FILTER_VALIDATE_INT);
+        $date = htmlspecialchars(trim($_POST["date"]), ENT_QUOTES, 'UTF-8');
+        $time = htmlspecialchars(trim($_POST["time"]), ENT_QUOTES, 'UTF-8');
         $sql="insert into schedule (docid,title,scheduledate,scheduletime,nop) values ($docid,'$title','$date','$time',$nop);";
         $result= $database->query($sql);
-        header("location: schedule.php?action=session-added&title=$title");
+        header("location: schedule.php?action=session-added&title=" . urlencode($title));
         
     }
 
